e140c7a0becb8bfb9a07833e51b7ebbb9c02ee99
Jpriya28 <jeyap344@gmail.com>
2025-01-11 15:49:51 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,122 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. HYPER TEXT MARKUP LANGUAGE",
+        options: [
+            "A. HYPER TYPE MULTI LANGUAGE",
+            "B. HYPER TEXT MULTIPLE LANGUAGE",
+            "C. HYPER TEXT MARKUP LANGUAGE",
+            "D. HOME TEXT MULTI LANGUAGE"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Who is making the Web standards?",
+        answer: "D. THE WORLD WIDE WEB CONSORTIUM",
+        options: [
+            "A. MICROSOFT",
+            "B. MOZILLA",
+            "C. GOOGLE CHROME",
+            "D. THE WORLD WIDE WEB CONSORTIUM"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "The HTML global attribute,contenteditable is used to?",
+        answer: "D. SPECIFY WHETHER THE CONTENT OF AN ELEMENT SHOULD BE EDITABLE OR NOT",
+        options: [
+            "A. UPDATE CONTENT FROM THE SERVER",
+            "B. SPECIFIES A CONTEXT MENU FROM THE SERVER",
+            "C. RETURN THE POSITION OF THE FIRST FOUND OCCURANCE OF CONTENT INSIDE THE STRING",
+            "D. SPECIFY WHETHER THE CONTENT OF AN ELEMENT SHOULD BE EDITABLE OR NOT"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "The HTML canvas element is used to?",
+        answer: "C. DRAW GRAPHICS",
+        options: [
+            "A. DISPLAYS DATABASE RECORDS",
+            "B. CREATE DRAGGABLE ELEMENTS",
+            "C. DRAW GRAPHICS",
+            "D. MANIPULATE DATA IN MYSQL"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does CSS stand for?",
+        answer: "C. CASCADING STYLE SHEETS",
+        options: [
+            "A. COMPUTER STYLE SHEETS",
+            "B. COLORFUL STYLE SHEETS",
+            "C. CASCADING STYLE SHEETS",
+            "D. CREATIVE STYLE SHEETS"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Which is the correct CSS syntax?",
+        answer: "A. body{color:black;}",
+        options: [
+            "A. body{color:black;}",
+            "B. {body:color=black;}",
+            "C. {body;color;black;}",
+            "D. body:color=black;"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "How do you make each word in a text start with a capital letter?",
+        answer: "B. text-transorm:Capitalize",
+        options: [
+            "A. text-style:Capitalize",
+            "B. text-transorm:Capitalize",
+            "C. You can't do that with CSS",
+            "D. transform:Capitalize"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+        answer: "A. if(i!=5)",
+        options: [
+            "A. if(i!=5)",
+            "B. if(i<>5)",
+            "C. if i<>5",
+            "D. if i=!then"
+
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answer: "D. Math.round(7.25)",
+        options: [
+            "A. round(7.25)",
+            "B. rnd(7.25)",
+            "C. Math.rnd(7.25)",
+            "D. Math.round(7.25)"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "How does a FOR loop start?",
+        answer: "A. for(i=0;i<=5;i++)",
+        options: [
+            "A. for(i=0;i<=5;i++)",
+            "B. for i=1to5;",
+            "C. for(i<=5;i++)",
+            "D. for(i=0;i<=5)"
+        ]
+    }
+];
